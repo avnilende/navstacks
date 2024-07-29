71e348ca051c3656092fdc878abcbbea7a580628
Avni Lende <armadillolegs@gmail.com>
2024-07-29 09:56:03 -0400

    Initial Commit

---
+++ navstacks/navstacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  navstacksApp.swift
+//  navstacks
+//
+//  Created by scholar on 7/29/24.
+//
+
+import SwiftUI
+
+@main
+struct navstacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
